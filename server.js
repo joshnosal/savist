de9fbb6f14c0b863f153fedc54dfcd8b93ab3b85
joshnosal <josh.nosal@gmail.com>
2022-03-21 00:11:53 -0500

    Heroku edits v4

--- server.js
+++ server.js
@@ -24,10 +24,10 @@ app.use('/user', userRouter)
 app.use('/stripe', stripeRouter)
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
+  app.use(express.static(path.resolve(__dirname, "./client/build")))
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-  });
+    res.sendFile(path.resolve(__dirname,  "./client/build", "index.html"))
+  })
 }
 
 mongoose.connect(process.env.DATABASE_URL, {
